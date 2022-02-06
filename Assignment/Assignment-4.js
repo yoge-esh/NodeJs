3fd5ea542c4df8af32451f05eb83ac02a213801a
yoge-esh <yogesh124khanal@gmail.com>
2022-02-06 15:00:42 +0545

    assignment-4

--- Assignment/Assignment-4.js
+++ Assignment/Assignment-4.js
@@ -1,3 +1,25 @@
+let car = [
+    {
+        "name": "BMW",
+        "model": "X5",
+        year: "2020",
+        add: function (name, model, year) {
+            this['name'].push(name);
+            this['model'].push(model);
+            this['year'].push(year);
+        }
+    },
+    {
+        name: "Audi",
+        model: "A4",
+        year: "2020",
+        add: function (name, model, year) {
+            this['name'].push(name);
+            this['model'].push(model);
+            this['year'].push(year);
+        }
+    },
+]
 
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -5,26 +27,17 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-readCarDetails();
-
-function readCarDetails() {
-    let car = {}
-    rl.question('Are you a user? ', (answer) => {
-        if (answer === 'yes' || answer === 'y') {
-            rl.question('Enter a car model?: ', (carmodel) => {
-                car.carmodel = carmodel;
-                rl.question('Enter a car brand?: ', (carBrand) => {
-                    car.carBrand = carBrand;
-                    rl.question('Enter a cars making year?: ', (carYear) => {
-                        car.carYear = carYear;
-                        console.log(car);
-                        rl.close();
-                    });
-                });
-            });
-        } else {
-            console.log("You are not a User, you cant add the car's details");
+rl.question('Are you a User?', (answer) => printAnswer(answer));
+function printAnswer(answer) {
+    if (answer == 'yes') {
+        rl.question('What is your name? ', (answer) => {
+            car.add('lamborghini', 'aventador', '2020');
+            console.log(car)
+            console.log(`Hello ${answer}`);
             rl.close();
-        }
-    });
+        });
+    } else {
+        console.log("You are not a User, you cant add the car's details");
+        rl.close();
+    }
 }
