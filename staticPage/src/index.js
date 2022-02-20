7563e2105e9681014d2d3f2bc94635a4b66a2955
yoge-esh <yogesh124khanal@gmail.com>
2022-02-20 21:34:36 +0545

    added partials

--- staticPage/src/index.js
+++ staticPage/src/index.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 
 const staticPath = path.join(__dirname, "../public");
+const templateName = path.join(__dirname, "../templates/views"); // changing name from views to templates
+const partialsPath = path.join(__dirname, "../templates/partials"); // changing name from views to templates
 
 // to set the view engine 
 app.set("view engine", "hbs");
-
+app.set('views', templateName);
+hbs.registerPartials(partialsPath);
 app.use(express.static(staticPath));
 
+
 // template 
+// INDEX.HBS
 app.get("/", (req, res) =>{
     res.render("index", {
         user: "User",
     });
 })
+
+// ABOUT.HBS
+app.get("/about", (req, res) =>{
+    res.render("about");
+});
+
 
 app.listen(3000, () =>{
     console.log('Listening to port 3000');
